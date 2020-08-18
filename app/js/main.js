4d219f0b28d05869d3b3687e08ab31f86a5fa6df
PitonEgor <lyasovich90@mail.ru>
2020-08-18 11:53:57 +0300

    added author

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
 $(function(){
 
+
+    $('.products__slider-inner').slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      dots: true,
+      arrows: false
+    });
+
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+        spacing: "2px",
+      });
     
     var mixer = mixitup('.products__inner-box');
 });
